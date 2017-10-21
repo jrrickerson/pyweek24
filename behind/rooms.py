73e1b32abc88dbfedb04b9f5b8a25730658ee8ec
JR Rickerson <jrrickerson@gmail.com>
2017-10-20 21:36:38 -0400

    Room: Fix rendering of ruler and add toggle key

--- behind/rooms.py
+++ behind/rooms.py
@@ -1,3 +1,4 @@
+import math
 import sge
 from sge.gfx import Sprite, Color
 
@@ -8,28 +9,23 @@ class ScrollableLevel(sge.dsp.Room):
     def __init__(self, *args, player=None, ruler=False, **kwargs):
         self.scroll_speed = config.PLAYER_MOVE_SPEED
         self.controls = config.PLAYER_CONTROLS
-
+        self.ruler = ruler
         self.player = player
 
         super().__init__(*args, **kwargs)
         self.add(player)
 
-        if ruler:
-            self.add_ruler()
-
-    def add_ruler(self):
-        self.ruler = []
+    def show_ruler(self):
         font = sge.gfx.Font(size=20)
-        for x in range(0, self.width, 100):
-            mark_text = Sprite(name=None, width=100, height=100)
-            mark_text.draw_text(
-                font, '{}'.format(x), 0, 0,
+        view = self.views[0]
+        first_mark = int(math.floor(view.x / 100)) * 100
+        last_mark = int(math.ceil((view.x + view.width) / 100)) * 100
+        for x in range(first_mark, last_mark, 100):
+            self.project_text(
+                font, '{}'.format(x),
+                x, self.height - 50, 0,
                 color=sge.gfx.Color("red"),
                 halign="center", valign="middle")
-            mark = sge.dsp.Object(
-                x, self.height - 100, sprite=mark_text, tangible=False)
-            self.ruler.append(mark)
-            self.add(mark)
 
     def _control_pressed(self, control_name):
         """Return True if any of the keys mapped to the control_name is
@@ -52,5 +48,15 @@ class ScrollableLevel(sge.dsp.Room):
         self.views[0].x += (x_dir * moved)
         self.views[0].y += (y_dir * moved)
 
+    def event_key_press(self, key, char):
+        # Press R to toggle ruler
+        # Capture this here so we only capture a single event,
+        # not multiple across multiple frames
+        if key == 'r':
+            self.ruler = not self.ruler
+
     def event_step(self, time_passed, delta_multi):
         self.scroll_view(time_passed)
+
+        if self.ruler:
+            self.show_ruler()
