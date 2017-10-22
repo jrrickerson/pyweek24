5a4b28f9e94307ba5d86d013d03f9a7f485ade87
JR Rickerson <jrrickerson@gmail.com>
2017-10-22 00:29:59 -0400

    Platform: Fix dynamic platform generation

--- behind/objects.py
+++ behind/objects.py
@@ -5,20 +5,20 @@ from . import config
 
 
 class Platform(sge.dsp.Object):
-    def __init__(self, *args, left_sprite=None, right_sprite=None,
-                 middle_sprite=None, **kwargs):
-        super().__init__(*args, sprite=None, **kwargs)
+    def __init__(self, x, y, width, height, left_sprite=None,
+                 right_sprite=None, middle_sprite=None, **kwargs):
         self.left_sprite = left_sprite
         self.right_sprite = right_sprite
         self.middle_sprite = middle_sprite or kwargs.pop('sprite')
-        self.sprite = self.create_platform_sprite()
+        sprite = self.create_platform_sprite(width, height)
+        super().__init__(x, y, sprite=sprite, **kwargs)
         self.checks_collisions = False
 
-    def create_platform_sprite(self):
+    def create_platform_sprite(self, width, height):
         """Draw a dynamically sized platform sprite from a left, middle, and
         right sprite."""
         # Create an invisible rectangle the size of the platform
-        sprite = Sprite(name=None, self.width, self.height)
+        sprite = Sprite(name=None, width=width, height=height)
         offset = 0
         # Draw left end
         if self.left_sprite:
@@ -26,12 +26,12 @@ class Platform(sge.dsp.Object):
             offset = self.left_sprite.width
         # Tile middle sprite through the end of the platform
         if self.middle_sprite:
-            for x in range(offset, self.width, self.middle_sprite.width):
-                sprite.draw_sprite(left.middle_sprite, 0, x, 0)
+            for x in range(offset, width, self.middle_sprite.width):
+                sprite.draw_sprite(self.middle_sprite, 0, x, 0)
             offset = x
         # Draw right end
         if self.right_sprite:
             sprite.draw_sprite(
-                self.right_sprite, 0, self,width - self.right_sprite.width, 0)
+                self.right_sprite, 0, width - self.right_sprite.width, 0)
 
         return sprite
