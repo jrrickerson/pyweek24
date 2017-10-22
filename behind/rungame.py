d1394ba481347c1b935de5d3f5ee8ca28a54920e
JR Rickerson <jrrickerson@gmail.com>
2017-10-22 00:30:43 -0400

    Init: Add some test platforms to our test level

--- behind/rungame.py
+++ behind/rungame.py
@@ -5,6 +5,7 @@ from . import game
 from . import player
 from . import background
 from . import rooms
+from . import objects
 
 
 def initialize(config):
@@ -15,6 +16,10 @@ def initialize(config):
         fps=config.GAME_FPS,
         window_text=config.GAME_WINDOW_TITLE)
 
+    platform_left = sge.gfx.Sprite(**config.PLATFORM_SPRITES['left'])
+    platform_right = sge.gfx.Sprite(**config.PLATFORM_SPRITES['right'])
+    platform_middle = sge.gfx.Sprite(**config.PLATFORM_SPRITES['middle'])
+
     # there are two sides, 'front' and 'behind'
     bg_obj = background.Background('front')
 
@@ -23,7 +28,19 @@ def initialize(config):
         sge.game.width / 2,
         rooms.ScrollableLevel.floor - player.Player.HEIGHT,
     )
+    test_platforms = [
+        (500, sge.game.height / 3 * 2, 16 * 10, 16),
+        (700, sge.game.height / 2, 16 * 20, 16),
+        (1500, sge.game.height / 4 * 3, 16 * 3, 16),
+        (1700, sge.game.height / 3 * 2, 16 * 6, 16),
+    ]
+    platforms = [objects.Platform(
+        x, y, w, h,
+        left_sprite=platform_left, middle_sprite=platform_middle,
+        right_sprite=platform_right) for x, y, w, h in test_platforms]
+
     sge.game.start_room = rooms.ScrollableLevel(
+        objects=platforms,
         player=player_obj, width=10000, background=bg_obj, ruler=False)
     sge.game.mouse_visible = False
 
